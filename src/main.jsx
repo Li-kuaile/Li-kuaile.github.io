d0bc0162be7b5990df55757e49fd37f28c270ad3
601495955@qq.com <601495955@qq.com>
2024-04-26 19:04:11 +0800

    refresh

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter,HashRouter } from "react-router-dom"
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
