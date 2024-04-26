e1bd405b58247e58aaffac398874d701475d593f
601495955@qq.com <601495955@qq.com>
2024-04-26 20:35:39 +0800

    refresh

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { BrowserRouter,HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // github pages刷新页面报错（404）,改成HashRouter
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 )
